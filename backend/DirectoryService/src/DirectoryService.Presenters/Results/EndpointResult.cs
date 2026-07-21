4f7fe2f05f218a0bb8fcdb1300c470fd963b8da6
G_Perboodag <gasanalievp@gmail.com>
2026-07-21 19:13:35 +0300

    feat: refactor exception handling and introduce EndpointResult for improved response management chore: update package references and remove unused exception classes

---
+++ backend/DirectoryService/src/DirectoryService.Presenters/Results/EndpointResult.cs
@@ -0,0 +1,45 @@
+﻿using CSharpFunctionalExtensions;
+using Microsoft.AspNetCore.Http;
+using Shared;
+using IResult = Microsoft.AspNetCore.Http.IResult;
+
+namespace DirectoryService.Presenters.Results;
+
+public class EndpointResult<TValue> : IResult
+{
+    private readonly IResult _result;
+
+    public EndpointResult(Result<TValue, Failure> result)
+    {
+        _result = result.IsSuccess
+            ? new SuccessResult<TValue>(result.Value)
+            : new FailureResult(result.Error);
+
+    }
+
+    public EndpointResult(Result<TValue, Error> result)
+    {
+        _result = result.IsSuccess
+            ? new SuccessResult<TValue>(result.Value)
+            : new FailureResult(result.Error);
+
+    }
+
+    public Task ExecuteAsync(HttpContext httpContext) =>
+            _result.ExecuteAsync(httpContext);
+
+    public static implicit operator EndpointResult<TValue>(Result<TValue, Failure> result) => new(result);
+
+    public static implicit operator EndpointResult<TValue>(Result<TValue, Error> result) => new(result);
+}
+
+public class EndpointResult : EndpointResult<bool>
+{
+    public EndpointResult(UnitResult<Failure> result) : base(result.IsSuccess ? true : result.Error) { }
+    public EndpointResult(UnitResult<Error> result) : base(result.IsSuccess ? true : result.Error) { }
+
+
+    public static implicit operator EndpointResult(UnitResult<Failure> result) => new(result);
+
+    public static implicit operator EndpointResult(UnitResult<Error> result) => new(result);
+}
---
+++ backend/Shared/Envelope.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Shared;
+
+public record Envelope<T>
+{
+    public T? Result { get; }
+
+    public Failure? Errors { get; }
+
+    public DateTime TimeGenerated { get; }
+
+    [JsonIgnore]
+    public bool IsError => Errors != null && Errors.Any();
+
+    [JsonConstructor]
+    internal Envelope(T? result, Failure? errors)
+    {
+        Result = result;
+        Errors = errors;
+        TimeGenerated = DateTime.Now;
+    }
+}
+
+public record Envelope : Envelope<object>
+{
+    [JsonConstructor]
+    internal Envelope(object? result, Failure? errors) : base(result, errors)
+    {
+    }
+
+    public static Envelope Ok(object result) =>
+        new(result, null);
+
+    public static Envelope<T> Ok<T>(T result) =>
+        new Envelope<T>(result, null);
+
+
+    public static Envelope Failure(Failure errors) =>
+        new(null, errors);
+
+    public static Envelope<T> Failure<T>(Failure errors) =>
+        new Envelope<T>(default, errors);
+}

959bdf072bc6d8076dc916096deb0c6fa6270ac6
G_Perboodag <gasanalievp@gmail.com>
2026-07-28 00:13:53 +0300

    feat: integrate Serilog for enhanced logging and monitoring capabilities

--- backend/DirectoryService/src/DirectoryService.Web/Program.cs
+++ backend/DirectoryService/src/DirectoryService.Web/Program.cs
@@ -2,35 +2,58 @@ using DirectoryService.Core;
 using DirectoryService.Infrastructure.Postgres;
 using DirectoryService.Web.Middlewares;
 using Scalar.AspNetCore;
+using Serilog;
+using System.Globalization;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .WriteTo.Console(formatProvider: CultureInfo.InvariantCulture)
+    .CreateBootstrapLogger();
 
-
-builder.Services.AddOpenApi();
-
-builder.Services.AddControllers();
-
-builder.Services.AddHealthChecks();
-
-builder.Services.AddInfrastructurePostgres(builder.Configuration);
-builder.Services.AddCore(builder.Configuration);
-
-builder.Services.AddScoped<ExceptionMiddleware>();
-
-var app = builder.Build();
-
-app.UseMiddleware<ExceptionMiddleware>();
-
-app.MapControllers();
-
-app.UseHealthChecks("/health");
-
-
-if(!app.Environment.IsProduction())
+try
 {
-    app.MapOpenApi();
-    app.MapScalarApiReference();
+    var builder = WebApplication.CreateBuilder(args);
+
+
+    builder.Services.AddOpenApi();
+
+    builder.Services.AddControllers();
+
+    builder.Services.AddHealthChecks();
+
+    builder.Services.AddInfrastructurePostgres(builder.Configuration);
+    builder.Services.AddCore(builder.Configuration);
+
+    builder.Services.AddScoped<ExceptionMiddleware>();
+
+    builder.Services.AddSerilog((services, lc) => lc
+        .ReadFrom.Configuration(builder.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.WithProperty("ServiceName", "DirectoryService"));
+
+    var app = builder.Build();
+
+    app.UseMiddleware<ExceptionMiddleware>();
+
+    app.MapControllers();
+
+    app.UseHealthChecks("/health");
+
+
+    if (!app.Environment.IsProduction())
+    {
+        app.MapOpenApi();
+        app.MapScalarApiReference();
+    }
+
+    await app.RunAsync();
+
 }
-
-
-await app.RunAsync();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
